94bbe04e64ebfbaa56d8e12e45f3f9320272736d
Carl Alquisola <carl.alquisola@epycpoint.com>
2025-06-14 22:47:07 -0400

    add merge two sorted lists

---
+++ src/main/java/MergeTwoSortedLists.java
@@ -0,0 +1,56 @@
+public class MergeTwoSortedLists {
+    public static void main(String[] args) {
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        System.out.println(mergeTwoLists(list1, list2));
+
+    }
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode prehead = new ListNode(-1);
+
+        ListNode prev = prehead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+
+        // At least one of l1 and l2 can still have nodes at this point, so connect
+        // the non-null list to the end of the merged list.
+        prev.next = l1 == null ? l2 : l1;
+
+        return prehead.next;
+
+    }
+
+    public static ListNode mergeTwoListsRecursion(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+
+        if (l2 == null) {
+            return l1;
+        }
+
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2;
+
+    }
+}
